110ed9ee393350ff4e6134f09113f29cff3862b7
Miguel Delgadillo <jdelgadilloh87@gmail.com>
2024-09-05 12:28:13 -0600

    Changes

--- quarkus-camel-template/skeleton/src/main/java/${{values.javaPackageName}}/Fruit.java
+++ quarkus-camel-template/skeleton/src/main/java/${{values.javaPackageName}}/Fruit.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package ${{ values.package }};
+package ${{ values.javaPackageName }};
 
 import java.util.Objects;
 
